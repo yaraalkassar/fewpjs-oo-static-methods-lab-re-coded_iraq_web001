6d883936bb6a94941583fcf2b54b6bcd247e82ac
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-17 12:52:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Formatter {
     return string.charAt(0).toUpperCase() + string.substring(1);
   }
   static sanitize(string){
-return string.replace(/[^a-zA-Z 0-9]+/g,' ');
+return string.replace(/[^A-Za-z0-9-' ]+/g, '')
   }
   static titleize(){
 
